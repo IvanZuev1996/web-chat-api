9d1051d621d3dcf26167efe3912984742933bc70
Ivan Zuev <ivan-zuev-97@mail.ru>
2023-11-02 11:27:23 +0300

    add vercel.json

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,12 +1,25 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const Pool = pg.Pool;
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'root',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'web_chat_postgres'
+// });
+
 const pool = new Pool({
-    user: 'postgres',
-    password: 'root',
-    host: 'localhost',
-    port: 5432,
-    database: 'web_chat_postgres'
+    connectionString: process.env.POSTGRES_URL + '?sslmode=require'
+});
+
+pool.connect((err: any) => {
+    if (err) throw new Error(err);
+    console.log('Connect to postgreSQL successfully');
 });
 
 export default pool;
